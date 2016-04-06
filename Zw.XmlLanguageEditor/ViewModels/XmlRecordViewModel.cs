32b22c05096554bb2027f4ee0a487e0245e7c853
ZoolWay <zoolway@gmail.com>
2016-04-06 21:00:20 +0200

    Added missing record view model to repository (how can it have been missing?)

---
+++ Zw.XmlLanguageEditor/ViewModels/XmlRecordViewModel.cs
@@ -0,0 +1,33 @@
+﻿using Caliburn.Micro;
+using System;
+using System.Collections.Generic;
+
+namespace Zw.XmlLanguageEditor.ViewModels
+{
+    public class XmlRecordViewModel : PropertyChangedBase
+    {
+        private List<string> secondaryValues;
+
+        public string Id { get; set; }
+        public string MasterValue { get; set; }
+
+            public string this[int secondaryIndex]
+        {
+            get
+            {
+                return (secondaryIndex < secondaryValues.Count) ? secondaryValues[secondaryIndex] : null;
+            }
+            set
+            {
+                while (secondaryIndex >= secondaryValues.Count) secondaryValues.Add(null); // fill with emty entries if needed
+                secondaryValues[secondaryIndex] = value;
+            }
+        }
+
+        public XmlRecordViewModel()
+        {
+            this.secondaryValues = new List<string>();
+        }
+
+    }
+}
